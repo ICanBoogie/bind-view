0a2d7040e5ed83304a3f19ff4743fc3a1bc1b7c4
Olivier Laviale <olivier.laviale@gmail.com>
2015-08-23 02:49:46 +0200

    Prototype config fragments are now their own file

---
+++ config/prototype.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ICanBoogie\Binding\View;
+
+use ICanBoogie;
+
+$hooks = Hooks::class . '::';
+
+return [
+
+	ICanBoogie\Routing\Controller::class . '::lazy_get_view' => $hooks . 'controller_get_view',
+	ICanBoogie\Routing\Controller::class . '::get_template' => $hooks . 'get_template',
+	ICanBoogie\Routing\Controller::class . '::get_layout' => $hooks . 'get_layout',
+	ICanBoogie\Routing\Route::class . '::lazy_get_template' => $hooks . 'get_template',
+	ICanBoogie\Routing\Route::class . '::lazy_get_layout' => $hooks . 'get_layout',
+	ICanBoogie\View\View::class . '::lazy_get_engines' => $hooks . 'view_lazy_get_engines',
+	ICanBoogie\View\View::class . '::lazy_get_template_resolver' => $hooks . 'view_lazy_get_template_resolver',
+
+];
