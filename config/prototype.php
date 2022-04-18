a6404ed5fd58d2e074f335c4681cfaae9581bd6c
Olivier Laviale <olivier.laviale@gmail.com>
2022-04-18 23:42:06 +0200

    Use callable notation for prototypes

--- config/prototype.php
+++ config/prototype.php
@@ -3,18 +3,12 @@
 namespace ICanBoogie\Binding\View;
 
 use ICanBoogie;
+use ICanBoogie\Routing\ControllerAbstract;
 
-$hooks = Hooks::class . '::';
-
-/**
- * @uses Hooks::controller_get_view()
- * @uses Hooks::get_template()
- * @uses Hooks::get_layout()
- */
 return [
 
-	ICanBoogie\Routing\ControllerAbstract::class . '::lazy_get_view' => $hooks . 'controller_get_view',
-	ICanBoogie\Routing\ControllerAbstract::class . '::get_template' => $hooks . 'get_template',
-	ICanBoogie\Routing\ControllerAbstract::class . '::get_layout' => $hooks . 'get_layout',
+	ControllerAbstract::class . '::lazy_get_view' => [ Hooks::class, 'controller_get_view' ],
+	ControllerAbstract::class . '::get_template' => [ Hooks::class, 'get_template' ],
+	ControllerAbstract::class . '::get_layout' => [ Hooks::class, 'get_layout' ],
 
 ];
