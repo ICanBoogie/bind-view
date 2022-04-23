53067704c19a596127613205daf95a775b9cf28e
Olivier Laviale <olivier.laviale@gmail.com>
2022-04-23 14:42:08 +0200

    Use prototype's ConfigBuilder

--- config/prototype.php
+++ config/prototype.php
@@ -5,10 +5,7 @@ namespace ICanBoogie\Binding\View;
 use ICanBoogie;
 use ICanBoogie\Routing\ControllerAbstract;
 
-return [
-
-	ControllerAbstract::class . '::lazy_get_view' => [ Hooks::class, 'controller_get_view' ],
-	ControllerAbstract::class . '::get_template' => [ Hooks::class, 'get_template' ],
-	ControllerAbstract::class . '::get_layout' => [ Hooks::class, 'get_layout' ],
-
-];
+return fn(ICanBoogie\Binding\Prototype\ConfigBuilder $config) => $config
+	->bind(ControllerAbstract::class, 'lazy_get_view', [ Hooks::class, 'controller_get_view' ])
+	->bind(ControllerAbstract::class, 'get_template', [ Hooks::class, 'get_template' ])
+	->bind(ControllerAbstract::class, 'get_layout', [ Hooks::class, 'get_layout' ]);
